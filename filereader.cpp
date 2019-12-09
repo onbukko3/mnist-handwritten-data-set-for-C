0ee4c015f8eeca082382c0031213b195285cffdc
utpjb <utpjb@student.kit.edu>
2019-12-09 15:19:41 +0100

    no idear

--- filereader.cpp
+++ filereader.cpp
@@ -29,70 +29,62 @@ int nofFileLines (char *filename)
     return line;
 }
 
-int ArrayList (char *filename, int linesize)
+void ArrayList (char *filename, int linesize)
 {
     FILE * pFile = NULL;
     int nread;
-    char * arrList[linesize];
+    char **arrList;
+    int i;
+    int j;
+    //이차원 배열 동적할당을 할때는 이런식으로 할것.
+    arrList = (char**)(malloc(sizeof(char)*linesize));
+    for(i =0; i<linesize;i++){
+        arrList[i] = (char*)malloc(sizeof(char)*10);
+    }
     int n = 0;
     char c;
-    char namestring[10];
-
+    
     pFile = fopen(filename, "r");
     if(pFile!=NULL) {
         while(1) {
-            //nread = fread(buf, sizeof(char), BUFFER_SIZE, pFile);
-            
-            //printf("%d",nread);
-            
-            //if(nread>0){
-            //    char* newstrptr = (char*)malloc(sizeof(char)*(nread));
-            //    for(int i =0; i<BUFFER_SIZE;i++){
-            //        strcpy(newstrptr, buf);
-            //        arrList[n] = newstrptr;
-            //        n++;}
-            //}
-            //else if (nread <= 0) break;
-            for(int i= 0 ; i<sizeof(namestring)/sizeof(char); i++){
-                
+            for(j= 0 ; j<sizeof(buf)/sizeof(char); j++){
                 c = fgetc(pFile);
                 if(c!='\n'){
-                namestring[i] = c;
-                char* newcharptr = (char*)malloc(sizeof(char)*linesize+1);
-                strcpy(newcharptr,namestring);
-                arrList[n] = newcharptr;
-                n++;}
+                buf[j] = c;
+                }
                 else if (c == '\n')
                 {
                     break;
-                }
-                
-                }
+                }}
             
-        fclose(pFile);    
-        }
+            arrList[n] = buf;
+            n++;          
+                
+            
+            
+        }fclose(pFile);
         
 
         for(n=0;n<linesize;n++){
             printf("%02d: %s\n",n, arrList[n]);
             free(arrList[n]);
         }
+        free(*arrList);
     }
-    return 0;
 }
 
 int main(){
-    char fn[]= "male.txt";
+    char fn[]= "coref/male.txt";
     int noline;
     char maleList;
 
     
     noline =  nofFileLines(fn);
-    maleList = ArrayList(fn, noline);
+    //maleList = ArrayList(fn, noline);
      
     printf("%s %d\n ", "The number of lines:", noline);
-    //printf("%s\n", maleList);
-
+    ArrayList(fn, noline);
+    
     return 0;
 
 }
