fa9f0c452cf90e83f473e06f59a92424c8b3f2ff
Seoncheol Kim <skim@cinemo.com>
2020-01-14 12:45:09 +0100

    ready to implementation environment

---
+++ include/linkedlist.h
@@ -0,0 +1,20 @@
+#ifndef _LINKEDLIST_H_
+#define _LINKEDLIST_H_
+
+typedef struct __list{
+    struct __node *cur;
+    struct __node *head;
+    struct __node *tail;    
+} linkedList;
+
+typedef struct __node{
+    char *data;
+    struct __node *next; // the address of the node
+} node;
+
+void createNode(linkedList *L, char *initData);
+void deleteLastNode(linkedList *L);
+void printNode(linkedList *L);
+void deleteAllNode(linkedList *L);
+
+#endif
